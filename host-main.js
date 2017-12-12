d5ccad91646f062b3365d53371bdf16b7449ada8
Gabriel Wu <gabrielwu84@gmail.com>
2017-12-12 21:19:51 +0800

    updated socketio-server address

--- host-main.js
+++ host-main.js
@@ -8,7 +8,9 @@ require.config({
 	paths:{
 		"jquery":"https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min",
 		"bootstrap":"https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min",
-		"socketio-server":"https://avalon-gabrielwu84.rhcloud.com/socket.io/socket.io",
+		// set default socketio-server address. Can be changed in kernelParams. 
+		// "socketio-server":"https://avalon-gabrielwu84.rhcloud.com/socket.io/socket.io",
+		"socketio-server":"http://socketio-server-youvote.a3c1.starter-us-west-1.openshiftapps.com/socket.io/socket.io.js",
 		"d3js":"https://cdnjs.cloudflare.com/ajax/libs/d3/4.2.0/d3.min",
 		"vue":"https://cdnjs.cloudflare.com/ajax/libs/vue/2.3.3/vue",
 		"lessonctrl":"lessonctrl",
